3c2b19b3420d37fdaca724b2755b1cddf8aa2d04
Rafael Costa <rafael_costa@made2web.com>
2022-11-11 16:11:54 +0000

    Contador adicionado

--- src/Templates/Home/index.jsx
+++ src/Templates/Home/index.jsx
@@ -1,76 +1,29 @@
+import { Component } from "react";
 import "./styles.css";
-import { useState } from "react";
-import { loadPosts } from "../../utils/load-posts";
-import { Posts } from "../../Components/Posts/Index";
-import { Button } from "../../Components/Button";
-import { TextInput } from "../../Components/TextInput";
-import { useEffect } from "react";
-import { useCallback } from "react";
 
-export const Home = () => {
-  const [posts, setPosts] = useState([]);
-  const [allPosts, setAllPosts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [postsPerPage] = useState(10);
-  const [searchValue, setSearchValue] = useState("");
-
-  const noMorePosts = page + postsPerPage >= allPosts.length;
-
-  const filteredPosts = !!searchValue
-    ? allPosts.filter((post) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase());
-      })
-    : posts;
-
-  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
-    const postsAndPhotos = await loadPosts();
-
-    setPosts(postsAndPhotos.slice(page, postsPerPage));
-    setAllPosts(postsAndPhotos);
-  }, []);
-
-  useEffect(() => {
-    console.log(new Date().toLocaleDateString("pt-pt"));
-    handleLoadPosts(0, postsPerPage);
-  }, [handleLoadPosts, postsPerPage]);
-
-  function loadMorePosts() {
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    posts.push(...nextPosts);
-
-    setPosts(posts);
-    setPage(nextPage);
-  }
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setSearchValue(value);
+export class Home extends Component {
+  state = {
+    counter: 0,
   };
 
-  return (
-    <section className="container">
-      <div className="search-container">
-        {!!searchValue && <h1>Search value: {searchValue}</h1>}
-        <TextInput
-          searchValue={searchValue}
-          handleChange={handleChange}
-        ></TextInput>
+  handleClick = () => {
+    this.setState(
+      (prevState, prevProps) => {
+        return { counter: prevState.counter + prevProps.numberToIncrement };
+      },
+
+      () => {
+        console.log("POST", this.state.counter);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleClick}>Increment</button>
       </div>
-
-      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-
-      {filteredPosts.length === 0 && <p>Não existem Posts</p>}
-
-      <div className="button-container"></div>
-
-      {!searchValue && (
-        <Button
-          disabled={noMorePosts}
-          text="load more posts"
-          onClick={loadMorePosts}
-        />
-      )}
-    </section>
-  );
-};
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Home } from "./Templates/Home";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Home numberToIncrement={1} />
   </React.StrictMode>
 );
 
